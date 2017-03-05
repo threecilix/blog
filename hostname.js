cb50ba67df21ebe3143f6b6ca981b9efa5aaf131
threecilix <trie.chilix@gmail.com>
2017-03-05 11:29:41 +0700

    Create hostname.js

---
+++ hostname.js
@@ -0,0 +1,1 @@
+var blog=document.location.hostname;var slug=document.location.pathname;var ctld=blog.substr(blog.lastIndexOf("."));if(ctld!=".com"){var ncr="http://"+blog.substr(0,blog.indexOf("."));ncr+=".blogspot.com/ncr"+slug;window.location.replace(ncr)}if(self != top) { top.location = self.location; }
